c27363fd20dcc02b28b0cdb2e32f44beb9aa09e6
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2022-01-14 15:48:17 -0300

    Unit tests

---
+++ Unit-Testing-Course/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class MathTests
+    {
+        [TestMethod]
+        public void Add_ReturnsAplusB()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 1);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Max_ReturnsMaxValue()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void GetOddNumbers_ReturnOddNumbers()
+        {
+            var math = new Math();
+            List<int> numbers = new List<int> { 1, 3, 5 };
+
+            var result = math.GetOddNumbers(5);
+
+            Assert.IsTrue(result.SequenceEqual(numbers));
+        }
+
+
+
+    }
+}
---
+++ Unit-Testing-Course/TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Framework")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Framework 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d119ef8-f022-421e-b16a-c58e7befe5f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
